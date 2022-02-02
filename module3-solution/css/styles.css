d2a3e16171ec6f80915acfdd7e53160f5bb0c71d
frdrrhr <40151057+frdrrhr@users.noreply.github.com>
2022-02-02 01:52:41 +0100

    Update styles.css

--- module3-solution/css/styles.css
+++ module3-solution/css/styles.css
@@ -1,1 +1,76 @@
 
+body {
+  font-size: 16px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+/** HEADER **/
+#header-nav {
+	border-radius: 0;
+	border: 0;
+}
+.navbar-brand h1 { /* Restaurant name */
+  font-size: 1.5em;
+  color: black;
+  text-transform: uppercase;
+  font-weight: bold;
+  margin-top: 0;
+  margin-bottom: 0;
+}
+#nav-list {
+  margin-top: 0px;
+  border: 1px solid black;
+  font-weight: bold;
+}
+.navbar-header button.navbar-toggle, .navbar-header .icon-bar {
+  border: 1px solid black;
+}
+.navbar-header button.navbar-toggle {
+  clear: both;
+  margin-top: -40px;
+}
+#collapsable-nav a {
+	border: 1px solid black;
+	background-color: white;
+}
+/* END HEADER */
+
+/** MAIN **/
+#main-content h2 { /* Our Menu */
+	font-size: 2em;
+	color: black;
+	font-weight: bold;
+	padding-bottom: 20px;
+}
+section {
+  background-color: grey;
+  padding: 15px;
+  color: black;
+  height: 2000px;
+}
+#p1 {
+	text-align: center;
+	font-size: 1.5em;
+	color: black;
+	font-weight: bold;
+}
+/* END MAIN */
+
+@media (min-width: 768px) {
+	#header-nav {
+		border-radius: 0;
+		border: 0;
+		background-color: grey;
+		border-bottom: 2px solid black;
+	}
+}
+@media (max-width: 767px) {
+  /* Header */
+	#header-nav {
+		border-radius: 0;
+		border: 0;
+	}
+	.navbar .navbar-header {
+		background-color: grey;
+		border-bottom: 2px solid black;
+	}
+}
